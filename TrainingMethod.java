53fab22724202f596e4d0bd7fc1067fd27d3a6d0
Ahsoka <115781403+Ahsoka726@users.noreply.github.com>
2023-09-22 10:36:19 +0200

    Avancement entrainement2

--- TrainingMethod.java
+++ TrainingMethod.java
@@ -1,13 +1,15 @@
 public class TrainingMethod {
-
     public static void main(String[] args){
 
         Telephone samsung = new Telephone(24, "samsung s7", "samsung");
 
         Telephone iphone = new Telephone(15, "iphone", "iphone 14");
 
+        Marque Oppo = new Marque("Oppo 17");
+
         System.out.println(iphone.pixels);
         System.out.println(samsung.marque);
+        System.out.println(Oppo.nom);
 
     }
 }
@@ -37,7 +39,13 @@ class Marque{
 }
 
 /*
+
 myMethod() is the name of the method
+
 static means that the method belongs to the Main class and not an object of the Main class. You will learn more
+
 about objects and how to access methods through objects later in this tutorial.
-void means that this method does not have a return value. You will learn more about return values later in this chapter*/
+
+void means that this method does not have a return value. You will learn more about return values later in this chapter
+
+*/
